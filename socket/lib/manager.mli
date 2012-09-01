5fffeacc22a5773675a54781fe473bc5fbfe486c
crotsos <cr409@cl.cam.ac.uk>
2012-09-01 12:12:49 -0700

    modifications to make the openflow switch function with the mirage net library

--- socket/lib/manager.mli
+++ socket/lib/manager.mli
@@ -28,3 +28,10 @@ val get_udpv4 : t -> Lwt_unix.file_descr
 val register_udpv4_listener : t -> ipv4_addr option * int -> Lwt_unix.file_descr -> unit
 val get_udpv4_listener : t -> ipv4_addr option * int -> Lwt_unix.file_descr Lwt.t
 val get_intf : interface -> string
+
+val set_promiscuous: t -> id -> (id -> Cstruct.buf -> unit Lwt.t) ->
+  unit                                                              
+val inject_packet : t -> id -> Cstruct.buf -> unit Lwt.t            
+val get_intf_name : t -> id -> string 
+val get_intf_mac : t -> id -> string  
+
