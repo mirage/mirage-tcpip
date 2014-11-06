5f60f27df418dc2c5a8b9f6d623139d33a364aee
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2014-11-06 13:54:43 +0000

    Initial version of IPV6 signature

--- lib/ipv6_mirage.ml
+++ lib/ipv6_mirage.ml
@@ -1,3 +1,39 @@
+(*
+ * Copyright (c) 2014 Nicolas Ojeda Bar <n.oje.bar@gmail.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS l SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
+module type IP = sig
+  type ethif
+  type 'a io
+  type buffer
+  type ipaddr
+  type callback = src:ipaddr -> dst:ipaddr -> buffer -> unit io
+
+  type t
+
+  val id : t -> ethif
+  val input : t -> tcp:callback -> udp:callback -> default:(proto:int -> callback) -> buffer -> unit io
+  val connect : ethif -> [> `Ok of t] io
+  val get_gateways : t -> ipaddr list
+  val get_ips : t -> ipaddr list
+end
+
+module type IPV6 = sig
+  include IP with type ipaddr = Ipaddr.V6.t
+end
+
 let (>>=) = Lwt.(>>=)
 let (>|=) = Lwt.(>|=)
 
@@ -5,8 +41,8 @@ module Make (E : V2_LWT.ETHIF) (T : V2_LWT.TIME) (C : V2.CLOCK) = struct
   type ethif    = E.t
   type 'a io    = 'a Lwt.t
   type buffer   = Cstruct.t
-  type ipv6addr = Ipaddr.V6.t
-  type callback = src:ipv6addr -> dst:ipv6addr -> buffer -> unit Lwt.t
+  type ipaddr = Ipaddr.V6.t
+  type callback = src:ipaddr -> dst:ipaddr -> buffer -> unit Lwt.t
 
   type t =
     { ethif : E.t;
@@ -47,4 +83,15 @@ module Make (E : V2_LWT.ETHIF) (T : V2_LWT.TIME) (C : V2.CLOCK) = struct
     Ipv6.add_ip ~now:(Ipv6.Time.of_float @@ C.time ()) ~st:state.state ~nc:state.nc
       (Ipv6.Macaddr.link_local_addr (E.mac ethif)) >>= fun () ->
     Lwt.return (`Ok state)
+
+  let get_gateways state =
+    List.map fst state.state.Ipv6.router_list
+
+  let get_ips state =
+    let rec loop = function
+      | [] -> []
+      | (_, Ipv6.TENTATIVE _) :: rest -> loop rest
+      | (ip, (Ipv6.PREFERRED _ | Ipv6.DEPRECATED _)) :: rest -> ip :: loop rest
+    in
+    loop state.state.Ipv6.my_ips
 end
