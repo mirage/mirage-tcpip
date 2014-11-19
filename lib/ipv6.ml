e29a5c69c3253ca660aab52f549d82ee9e274f66
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2014-11-19 06:47:51 +0000

    ipv6: eschew polymorphic variants

--- lib/ipv6.ml
+++ lib/ipv6.ml
@@ -187,19 +187,25 @@ let allocate_pong state ~src ~dst ~id ~seq ~data =
 let float_of_uint32 n =
   Uint32.to_float (Uint32.of_int32 n)
 
+type ndp_option =
+  | SLLA of Macaddr.t
+  | TLLA of Macaddr.t
+  | MTU of int
+  | Prefix of Ndpv6.ra_prefix
+
 let rec fold_options f i opts =
   if Cstruct.len opts >= Ipv6_wire.sizeof_opt then
     (* TODO check for invalid len == 0 *)
     let opt, opts = Cstruct.split opts (Ipv6_wire.get_opt_len opts * 8) in
     match Ipv6_wire.get_opt_ty opt, Ipv6_wire.get_opt_len opt with
     | 1, 1 ->
-      let o = `SLLA (Macaddr.of_cstruct (Ipv6_wire.get_llopt_addr opt)) in
+      let o = SLLA (Macaddr.of_cstruct (Ipv6_wire.get_llopt_addr opt)) in
       fold_options f (f i o) opts
     | 2, 1 ->
-      let o = `TLLA (Macaddr.of_cstruct (Ipv6_wire.get_llopt_addr opt)) in
+      let o = TLLA (Macaddr.of_cstruct (Ipv6_wire.get_llopt_addr opt)) in
       fold_options f (f i o) opts
     | 5, 1 ->
-      let o = `MTU (Int32.to_int (Cstruct.BE.get_uint32 opt 4)) in
+      let o = MTU (Int32.to_int (Cstruct.BE.get_uint32 opt 4)) in
       fold_options f (f i o) opts
     | 3, 4 ->
       let prf_prefix =
@@ -212,13 +218,23 @@ let rec fold_options f i opts =
       let prf_valid_lifetime = span @@ Ipv6_wire.get_opt_prefix_valid_lifetime opt in
       let prf_preferred_lifetime = span @@ Ipv6_wire.get_opt_prefix_preferred_lifetime opt in
       let prf = {Ndpv6.prf_on_link; prf_autonomous; prf_valid_lifetime; prf_preferred_lifetime; prf_prefix} in
-      let o = `Prefix prf in
+      let o = Prefix prf in
       fold_options f (f i o) opts
     | ty, len ->
       Printf.printf "ND: ND option (ty=%d,len=%d) not supported in RA\n%!" ty len;
       fold_options f i opts
   else
     i
+
+type parse_result =
+  | Drop
+  | DropWithError of int * int * int
+  | Ndp of Ipaddr.V6.t * Ipaddr.V6.t * Ndpv6.packet
+  | Ping of Ipaddr.V6.t * Ipaddr.V6.t * int * int * Cstruct.t
+  | Pong of Cstruct.t
+  | Udp of Ipaddr.V6.t * Ipaddr.V6.t * Cstruct.t
+  | Tcp of Ipaddr.V6.t * Ipaddr.V6.t * Cstruct.t
+  | Default of int * Ipaddr.V6.t * Ipaddr.V6.t * Cstruct.t
 
 let parse_ra ~src ~dst buf =
   let ra_cur_hop_limit = Ipv6_wire.get_ra_cur_hop_limit buf in
@@ -229,13 +245,13 @@ let parse_ra ~src ~dst buf =
   let ra_slla, ra_prefix =
     fold_options begin fun ra opt ->
       match opt with
-      | `SLLA ra_slla     -> let _, ra_prefix = ra in Some ra_slla, ra_prefix
-      | `Prefix ra_prefix -> let ra_slla, _ = ra in ra_slla, Some ra_prefix
+      | SLLA ra_slla     -> let _, ra_prefix = ra in Some ra_slla, ra_prefix
+      | Prefix ra_prefix -> let ra_slla, _ = ra in ra_slla, Some ra_prefix
       | _ -> ra
     end (None, None) ra_opts
   in
   let ra = {Ndpv6.ra_cur_hop_limit; ra_router_lifetime; ra_reachable_time; ra_retrans_timer; ra_slla; ra_prefix} in
-  `Ndp (src, dst, Ndpv6.RA ra)
+  Ndp (src, dst, Ndpv6.RA ra)
 
 let parse_ns ~src ~dst buf =
   let ns_target = Ipaddr.V6.of_cstruct (Ipv6_wire.get_ns_target buf) in
@@ -243,11 +259,11 @@ let parse_ns ~src ~dst buf =
   let ns_slla =
     fold_options begin fun ns opt ->
       match opt with
-      | `SLLA ra_slla -> Some ra_slla
+      | SLLA ra_slla -> Some ra_slla
       | _ -> ns
     end None ns_opts
   in
-  `Ndp (src, dst, Ndpv6.NS {Ndpv6.ns_target; ns_slla})
+  Ndp (src, dst, Ndpv6.NS {Ndpv6.ns_target; ns_slla})
 
 let parse_na ~src ~dst buf =
   let na_router    = Ipv6_wire.get_na_router buf in
@@ -258,11 +274,11 @@ let parse_na ~src ~dst buf =
   let na_tlla      =
     fold_options begin fun na opt ->
       match opt with
-      | `TLLA na_tlla -> Some na_tlla
+      | TLLA na_tlla -> Some na_tlla
       | _ -> na
     end None na_opts
   in
-  `Ndp (src, dst, Ndpv6.NA {Ndpv6.na_router; na_solicited; na_override; na_target; na_tlla})
+  Ndp (src, dst, Ndpv6.NA {Ndpv6.na_router; na_solicited; na_override; na_target; na_tlla})
 
 (* buf : icmp packet with ipv6 header *)
 let parse_icmp ~src ~dst buf poff =
@@ -270,19 +286,19 @@ let parse_icmp ~src ~dst buf poff =
   let csum = checksum' ~proto:58 buf [ icmpbuf ] in
   if csum != 0 then
     let () = Printf.printf "ICMP6 checksum error (0x%x), dropping packet\n%!" csum in
-    `Drop
+    Drop
   else
     match Ipv6_wire.get_icmpv6_ty icmpbuf with
     | 128 -> (* Echo request *)
       let id = Cstruct.BE.get_uint16 icmpbuf 4 in
       let seq = Cstruct.BE.get_uint16 icmpbuf 6 in
-      `Ping (src, dst, id, seq, Cstruct.shift icmpbuf 8)
+      Ping (src, dst, id, seq, Cstruct.shift icmpbuf 8)
     | 129 (* Echo reply *) ->
-      `Pong (Cstruct.shift buf poff)
+      Pong (Cstruct.shift buf poff)
       (* Printf.printf "ICMP6: Discarding Echo Reply\n%!"; *)
     | 133 (* RS *) ->
       (* RFC 4861, 2.6.2 *)
-      `Drop
+      Drop
     | 134 (* RA *) ->
       parse_ra ~src ~dst icmpbuf
     | 135 (* NS *) ->
@@ -291,7 +307,7 @@ let parse_icmp ~src ~dst buf poff =
       parse_na ~src ~dst icmpbuf
     | n ->
       let () = Printf.printf "ICMP6: unrecognized type (%d)\n%!" n in
-      `Drop
+      Drop
 
 let parse_packet ~state buf =
   let src = Ipaddr.V6.of_cstruct (Ipv6_wire.get_ipv6_src buf) in
@@ -308,24 +324,24 @@ let parse_packet ~state buf =
       Printf.printf "Processing HOPOPT header\n%!";
       parse_options poff
     | 0 ->
-      `Drop
+      Drop
     | 60 (* IPv6-Opts *) ->
       Printf.printf "Processing DESTOPT header\n%!";
       parse_options poff
     | 43 (* IPv6-Route *) | 44 (* IPv6-Frag *) | 50 (* ESP *) | 51 (* AH *) | 135 (* Mobility Header *)
     | 59 (* NO NEXT HEADER *) ->
-      `Drop
+      Drop
     | 58 (* ICMP *) ->
       parse_icmp ~src ~dst buf poff
     | 17 (* UDP *) ->
-      `Udp (src, dst, Cstruct.shift buf poff)
+      Udp (src, dst, Cstruct.shift buf poff)
     | 6 (* TCP *) ->
-      `Tcp (src, dst, Cstruct.shift buf poff)
+      Tcp (src, dst, Cstruct.shift buf poff)
     | n when 143 <= n && n <= 255 ->
       (* UNASSIGNED, EXPERIMENTAL & RESERVED *)
-      `Drop
+      Drop
     | n ->
-      `Default (n, src, dst, Cstruct.shift buf poff)
+      Default (n, src, dst, Cstruct.shift buf poff)
 
   and parse_options poff =
     let pbuf = Cstruct.shift buf poff in
@@ -351,16 +367,16 @@ let parse_packet ~state buf =
             loop (ooff+len+2)
           | 0x40 ->
             (* discard the packet *)
-            `Drop
+            Drop
           | 0x80 ->
             (* discard, send icmp error *)
-            `DropWithError (4, 2, ooff)
+            DropWithError (4, 2, ooff)
           | 0xc0 ->
             (* discard, send icmp error if dest is not mcast *)
             if Ipaddr.V6.is_multicast dst then
-              `Drop
+              Drop
             else
-              `DropWithError (4, 2, ooff)
+              DropWithError (4, 2, ooff)
           | _ ->
             assert false
       else
@@ -372,22 +388,22 @@ let parse_packet ~state buf =
 
   if Ipaddr.V6.Prefix.(mem src multicast) then begin
     Printf.printf "Dropping packet, src is mcast\n%!";
-    `Drop
+    Drop
   end else if not (Ndpv6.is_my_addr state dst || Ipaddr.V6.Prefix.(mem dst multicast)) then begin
     Printf.printf "Dropping packet, not for me\n%!";
-    `Drop
+    Drop
   end else
     parse_extension true (Ipv6_wire.get_ipv6_nhdr buf) Ipv6_wire.sizeof_ipv6
 
 type action =
-  [ `Send  of Cstruct.t list
-  | `Sleep of float ]
+  | Send  of Cstruct.t list
+  | Sleep of float
 
 let rec run ~now ~state ~queued acts =
   let rec loop state queued acts = function
     | Ndpv6.Sleep dt :: rest ->
       Printf.printf "Sleeping for %.1fs\n%!" dt;
-      loop state queued (`Sleep dt :: acts) rest
+      loop state queued (Sleep dt :: acts) rest
     | Ndpv6.SendNS (src, dst, target) :: rest ->
       Printf.printf "Sending NS from %s to %s with target %s\n%!"
         (Ipaddr.V6.to_string src) (Ipaddr.V6.to_string dst) (Ipaddr.V6.to_string target);
@@ -412,7 +428,7 @@ let rec run ~now ~state ~queued acts =
       if IntMap.mem i queued then
         let datav = IntMap.find i queued in
         let queued = IntMap.remove i queued in
-        loop state queued (`Send (datav dmac) :: acts) rest
+        loop state queued (Send (datav dmac) :: acts) rest
       else
         (* FIXME log warning / error / assert false *)
         loop state queued acts rest
@@ -436,7 +452,7 @@ and output ~now (state, queued) ~dst frame datav =
   | Ndpv6.SendNow dmac ->
     Printf.printf "Sending packet to %s\n%!" (Ipaddr.V6.to_string dst);
     let (state, queued), acts = run ~now ~state ~queued acts in
-    (state, queued), `Send (datav dmac) :: acts
+    (state, queued), Send (datav dmac) :: acts
   | Ndpv6.SendLater i ->
     Printf.printf "Queueing packet #%d to %s\n%!" i (Ipaddr.V6.to_string dst);
     let queued = IntMap.add i datav queued in
@@ -445,17 +461,17 @@ and output ~now (state, queued) ~dst frame datav =
 let input ~now ((state, queued) as st) buf =
   let p = parse_packet ~state buf in
   match p with
-  | `Drop ->
+  | Drop ->
     `Drop
-  | `Ndp (src, dst, packet) ->
+  | Ndp (src, dst, packet) ->
     let state, acts = Ndpv6.input ~now ~state ~src ~dst packet in
     let st, acts = run ~now ~state ~queued acts in
     `Act (st, acts)
-  | `Default (proto, src, dst, pkt) ->
+  | Default (proto, src, dst, pkt) ->
     `Default (proto, src, dst, pkt)
-  | `DropWithError (ty, code, off) ->
+  | DropWithError (ty, code, off) ->
     `Drop (* TODO *)
-  | `Ping (src, dst, id, seq, data) ->
+  | Ping (src, dst, id, seq, data) ->
     Printf.printf "Received PING from %s to %s (id=%d,seq=%d)\n%!" (Ipaddr.V6.to_string src)
       (Ipaddr.V6.to_string dst) id seq;
     let dst = src
@@ -463,11 +479,11 @@ let input ~now ((state, queued) as st) buf =
     let frame, bufs = allocate_pong state ~src ~dst ~id ~seq ~data in
     let st, acts = output ~now st ~dst frame bufs in
     `Act (st, acts)
-  | `Pong buf ->
+  | Pong buf ->
     `Drop
-  | `Tcp (src, dst, pkt) ->
+  | Tcp (src, dst, pkt) ->
     `Tcp (src, dst, pkt)
-  | `Udp (src, dst, pkt) ->
+  | Udp (src, dst, pkt) ->
     `Udp (src, dst, pkt)
 
 let tick ~now (state, queued) =
@@ -523,10 +539,10 @@ module Make (E : V2_LWT.ETHIF) (T : V2_LWT.TIME) (C : V2.CLOCK) = struct
 
   and run t acts =
     Lwt_list.iter_s begin function
-      | `Sleep dt ->
+      | Sleep dt ->
         Lwt.ignore_result (T.sleep dt >>= fun () -> run_tick t);
         Lwt.return_unit
-      | `Send pkt ->
+      | Send pkt ->
         E.writev t.ethif pkt
     end acts
 
