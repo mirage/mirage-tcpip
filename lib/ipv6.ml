69c564dd0d74c1e5a7750fb9ad477d65424d76c0
Anil Madhavapeddy <anil@recoil.org>
2014-11-21 15:12:34 +0000

    Note a FIXME for a hardcoded sleep

--- lib/ipv6.ml
+++ lib/ipv6.ml
@@ -575,6 +575,7 @@ module Make (E : V1_LWT.ETHIF) (T : V1_LWT.TIME) (C : V1.CLOCK) = struct
     | `Default (proto, src, dst, pkt) -> default ~proto ~src ~dst pkt
 
   let connect ethif =
+    (* FIXME: remove this pause *)
     T.sleep 10.0 >>= fun () ->
     Printf.printf "Starting\n%!";
     let now = C.time () in
