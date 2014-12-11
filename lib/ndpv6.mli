d50cb927b609a6219e37a0abbd317b9b2b6c27f4
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2014-12-11 00:51:07 -0300

    Ipv6: Remove old docstrings
    More complete docstrings will be added later.

--- lib/ndpv6.mli
+++ lib/ndpv6.mli
@@ -14,66 +14,6 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-(** Neighbor Discovery Protocol and Stateless Adress Auto Configuration for IPv6.
-
-    [Ndpv6] implements the {{:http://tools.ietf.org/html/rfc4861}Neighbor
-    Discover Protocol} (NDP).  This protocol is used to find other IPv6 nodes in
-    the same local network, monitor any change in their reachability and also to
-    advertise our own existence to them.  In this way it plays a crucial role in
-    {em address resolution:} finding out suitable neighboring MAC hardware
-    addresses in order to route IPv6 packets.
-
-    It also implements the required functionality to support
-    {{:http://tools.ietf.org/html/rfc4862}Stateless Address Auto Configuration
-    for IPv6} (SLAAC).  This allows to negotiate, with the help of routers in
-    the local network, and obtain globally unique IPv6 addresses.  These
-    addresses are deduced from our own MAC address, a simple technique that is
-    not available in IPv4 given the relative sizes of both types of addresses
-    (48 bits for MAC vs 32 bits for IPv4).
-
-    Conceptually an IPv6 keeps 5 data structures associated with routing.
-
-    {ul
-    {- The Address List: this data structure keeps track of which addresses have
-       been assigned or are in the process of being assigned (via SLAAC) to the
-       network interface.  Addresses assigned via SLAAC have {em preferred} and
-       {em valid} lifetimes associated to them and this data structure must be
-       checked periodically to make sure that expired addresses are removed from
-       the list.}
-    {- The Prefix List: this is a list of "on-link" prefixes provided by routers
-       in the local network for the purposes of SLAAC.  These play the role of
-       the subnet mask of IPv4 and they are used to decide whether a packet must
-       be routed through a router or sent directly to the destination
-       address.}
-    {- The Neighbor Cache: this is the most important structure.  It keeps track
-       of every node that we have comunicated with (irrespective of whether the
-       communication was started by us or them) and their hardware address.  A
-       number of periodic upkeep tasks need to be performed in order to keep the
-       information in the cache up-to-date.}
-    {- The Router List: this is the list of known routers.  If an IPv6 packet is
-       not on-link (i.e., it is not prefixed by one of the elements of the
-       Prefix List), then it must be sent through a router.  This router will be
-       chosen in an appropiate way from this list.  Routers also have valid
-       lifetimes and care must be taken to expire them when they are no longer
-       valid.  Routers can advertise their existence by the use of Router
-       Advertisement messages.}}
-
-    {Implementation Notes}
-
-    All the data structures in this modules are immutable.  There is one
-    top-level module for each of the data structures listed previously.
-
-    {ul
-    {- [`Sleep dt] means sleeping [dt] seconds and then call the corresponding
-       [tick] function again.}
-    {- [`SendNS (sf, dst, tgt)] means sending a Neighbor Solicitation to [dst]
-       with target [tgt].  If [sf] is [`Unspecified], then the source address
-       should be [::].  If it is [`Specified], then it should be an assigned
-       address (selected using [select_source]).}
-    {- [`SendQueued (ip, mac)] means sending any queued packets waiting to be
-       delivered to [ip].  The MAC address of the destination field in these
-       packets should be set to [mac].}} *)
-
 module Ipaddr = Ipaddr.V6
 
 (** Address List management.
@@ -96,7 +36,7 @@ module AddressList : sig
       addresses. *)
 
   val select_source: t -> dst:Ipaddr.t -> Ipaddr.t
-  (** Source Selection according to RFC .... Since  *)
+  (** Source Selection *)
 
   val tick: t -> now:float -> retrans_timer:float ->
     t * [> `Sleep of float | `SendNS of [> `Unspecified ] * Ipaddr.t * Ipaddr.t ] list
@@ -132,9 +72,9 @@ module AddressList : sig
       globally unique address with prefix [pfx]. *)
 
   val handle_na: t -> Ipaddr.t -> t
-  (** [handle_na al ip] handles a NA which has arrived from [ip].  If [ip] is a
-      TENTATIVE address in [al] then it means that DAD has failed and [ip] should
-      not be bound. *)
+  (** [handle_na al ip] handles a Neighbor Advertisement which has arrived from
+      [ip].  If [ip] is a TENTATIVE address in [al] then it means that DAD has
+      failed and [ip] should not be bound. *)
 end
 
 module PrefixList : sig
