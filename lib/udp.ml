f0fdfe3d4c78514125dda29e793d4cf76ade624f
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2014-12-12 09:21:39 -0300

    Udp: Fix checksum calculation

--- lib/udp.ml
+++ lib/udp.ml
@@ -58,9 +58,9 @@ module Make(Ip: V1_LWT.IP) = struct
     let udp_buf = Cstruct.shift frame header_len in
     Wire_structs.set_udp_source_port udp_buf source_port;
     Wire_structs.set_udp_dest_port udp_buf dest_port;
+    Wire_structs.set_udp_length udp_buf (Wire_structs.sizeof_udp + Cstruct.lenv bufs);
     let csum = Ip.checksum frame (udp_buf :: bufs) in
     Wire_structs.set_udp_checksum udp_buf csum;
-    Wire_structs.set_udp_length udp_buf (Wire_structs.sizeof_udp + Cstruct.lenv bufs);
     let frame = Cstruct.set_len frame (header_len + Wire_structs.sizeof_udp) in
     Ip.writev t.ip frame bufs
 
