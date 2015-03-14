1e09eb1fc8d2033105e1044fb23fd6412581312c
Hannes Mehnert <hannes@mehnert.org>
2015-03-14 10:37:07 +0000

    the buffer length is already checked, Macaddr.of_bytes_exn will never raise

--- lib/wire_structs.ml
+++ lib/wire_structs.ml
@@ -15,10 +15,9 @@ let parse_ethernet_frame frame =
     (* minimum payload is 46 + source + destination + type *)
     let payload = Cstruct.shift frame sizeof_ethernet
     and typ = get_ethernet_ethertype frame
+    and dst = Macaddr.of_bytes_exn (copy_ethernet_dst frame)
     in
-    match Macaddr.of_bytes (copy_ethernet_dst frame) with
-    | Some destination_mac -> Some (int_to_ethertype typ, destination_mac, payload)
-    | None -> None
+    Some (int_to_ethertype typ, dst, payload)
   else
     None
 
