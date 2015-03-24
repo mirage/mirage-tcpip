82a6499c98477ee3b0869ed3f832b83427f45ae6
Mindy Preston <meetup@yomimono.org>
2015-03-24 00:49:03 +0000

    channel: extremely minimal unit tests added

---
+++ lib_test/test_channel.ml
@@ -0,0 +1,59 @@
+open Lwt
+
+(* this is a very small set of tests for the channel interface, intended to
+   ensure that EOF conditions on the underlying flow are handled properly *)
+module Channel = Channel.Make(Fflow)
+
+let cmp a b = 
+  match (String.compare a b) with | 0 -> true | _ -> false
+
+let printer = function
+  | `Success -> "success"
+  | `Failure s -> s
+
+let test_read_char_eof () =
+  let f = Fflow.make () in 
+  let c = Channel.create f in
+  let try_char_read () = 
+    Channel.read_char c >>= fun ch -> 
+    OUnit.assert_failure (Printf.sprintf "character %c was returned from
+    Channel.read_char on an empty flow" ch)
+  in
+  Lwt.try_bind 
+    (try_char_read) 
+    (fun () -> Lwt.return (`Failure "no exception" )) (* "success" case (no exceptions) *)
+    (function 
+      | Channel.End_of_file -> Lwt.return (`Success)
+      | e -> Lwt.return (`Failure (Printf.sprintf "wrong exception: %s"
+                                     (Printexc.to_string e)))
+    )
+
+let test_read_until_eof () =
+  let check a b = OUnit.assert_equal ~printer:(fun a -> a) ~cmp a
+      (Cstruct.to_string b) in
+  let input = Fflow.input_string "I am the very model of a modern major general"
+  in
+  let f = Fflow.make ~input () in
+  let c = Channel.create f in
+  Channel.read_until c 'v' >>= function
+  | true, buf -> 
+    check "I am the " buf;
+    Channel.read_until c '\xff' >>= fun (found, buf) ->
+    OUnit.assert_equal ~msg:"claimed we found a char that couldn't have been
+      there in read_until" false found;
+    check "ery model of a modern major general" buf;
+    Channel.read_until c '\n' >>= fun (found, buf) ->
+    OUnit.assert_equal ~msg:"claimed we found a char after EOF in read_until"
+      false found;
+    OUnit.assert_equal ~printer:string_of_int 0 (Cstruct.len buf);
+    Lwt.return_unit
+  | false, _ -> 
+    OUnit.assert_failure "thought we couldn't find a 'v' in input test"
+
+let _ =
+  Lwt_main.run (
+    test_read_char_eof () >>= fun res ->
+    OUnit.assert_equal ~printer `Success res;
+    test_read_until_eof () >>= fun () ->
+    Lwt.return_unit
+  )
