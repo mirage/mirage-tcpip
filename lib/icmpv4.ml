cf363da7d3fe34e3959e45feb3c77f4bbd92e25e
Mindy Preston <mindy.preston@docker.com>
2016-03-23 15:14:20 +0000

    Move Print and Parse modules out of Make functor in Icmpv4.
    Signed-off-by: Mindy Preston <mindy.preston@docker.com>

--- lib/icmpv4.ml
+++ lib/icmpv4.ml
@@ -1,66 +1,67 @@
 open Result
+
+module Parse = struct
+  (* second 4 bytes of the message have varying interpretations *)
+  type subheader =
+    | Id_and_seq of Cstruct.uint16 * Cstruct.uint16
+    | Pointer of Cstruct.uint8
+    | Address of Ipaddr.V4.t
+    | Unused
+
+  type t = {
+    code : Cstruct.uint8;
+    ty : Cstruct.uint8;
+    csum : Cstruct.uint16;
+    subheader : subheader;
+    payload : Cstruct.t option;
+  }
+
+  let subheader_of_cstruct ty buf =
+    let open Cstruct.BE in
+    match ty with
+    | 0 | 8 | 13 | 14 | 15 | 16 -> Id_and_seq (get_uint16 buf 0, get_uint16 buf 2)
+    | 3 | 11 | 4 -> Unused
+    | 5 -> Address (Ipaddr.V4.of_int32 (get_uint32 buf 0))
+    | 12 -> Pointer (Cstruct.get_uint8 buf 0)
+    | _ -> Unused
+
+  let input buf =
+    if Cstruct.len buf < Wire_structs.Ipv4_wire.sizeof_icmpv4 then
+      Error "packet too short for ICMPv4 header"
+    else begin
+      let open Wire_structs.Ipv4_wire in
+      let ty = get_icmpv4_ty buf in
+      let code = get_icmpv4_code buf in
+      let csum = get_icmpv4_csum buf in
+      let subheader = subheader_of_cstruct ty (Cstruct.shift buf 4) in
+      let payload =
+        if Cstruct.len buf > sizeof_icmpv4
+        then Some (Cstruct.shift buf sizeof_icmpv4)
+        else None
+      in
+      Ok { code; ty; csum; subheader; payload }
+    end
+
+end
+
+module Print = struct
+  let echo_request id seq =
+    let open Wire_structs.Ipv4_wire in
+    (* TODO: can this just be an appropriately-sized cstruct? *)
+    let buf = Io_page.(get 1 |> to_cstruct) in
+    let buf = Cstruct.set_len buf sizeof_icmpv4 in
+    set_icmpv4_ty buf 0x08;
+    set_icmpv4_code buf 0x00;
+    set_icmpv4_seq buf seq;
+    set_icmpv4_id buf id;
+    buf
+
+end
+
 module Make(IP : V1_LWT.IPV4) = struct
   type buffer = Cstruct.t
   type 'a io = 'a Lwt.t
   type ipaddr = Ipaddr.V4.t
-
-  module Parse = struct
-    (* second 4 bytes of the message have varying interpretations *)
-    type subheader =
-      | Id_and_seq of Cstruct.uint16 * Cstruct.uint16
-      | Pointer of Cstruct.uint8
-      | Address of Ipaddr.V4.t
-      | Unused
-
-    type t = {
-      code : Cstruct.uint8;
-      ty : Cstruct.uint8;
-      csum : Cstruct.uint16;
-      subheader : subheader;
-      payload : Cstruct.t option;
-    }
-
-    let subheader_of_cstruct ty buf =
-      let open Cstruct.BE in
-      match ty with
-      | 0 | 8 | 13 | 14 | 15 | 16 -> Id_and_seq (get_uint16 buf 0, get_uint16 buf 2)
-      | 3 | 11 | 4 -> Unused
-      | 5 -> Address (Ipaddr.V4.of_int32 (get_uint32 buf 0))
-      | 12 -> Pointer (Cstruct.get_uint8 buf 0)
-      | _ -> Unused
-
-    let input buf =
-      if Cstruct.len buf < Wire_structs.Ipv4_wire.sizeof_icmpv4 then
-        Error "packet too short for ICMPv4 header"
-      else begin
-        let open Wire_structs.Ipv4_wire in
-        let ty = get_icmpv4_ty buf in
-        let code = get_icmpv4_code buf in
-        let csum = get_icmpv4_csum buf in
-        let subheader = subheader_of_cstruct ty (Cstruct.shift buf 4) in
-        let payload =
-          if Cstruct.len buf > sizeof_icmpv4
-          then Some (Cstruct.shift buf sizeof_icmpv4)
-          else None
-        in
-        Ok { code; ty; csum; subheader; payload }
-      end
-
-  end
-
-  module Print = struct
-    let echo_request id seq =
-      let open Wire_structs.Ipv4_wire in
-      (* TODO: can this just be an appropriately-sized cstruct? *)
-      let buf = Io_page.(get 1 |> to_cstruct) in
-      let buf = Cstruct.set_len buf sizeof_icmpv4 in
-      set_icmpv4_ty buf 0x08;
-      set_icmpv4_code buf 0x00;
-      set_icmpv4_seq buf seq;
-      set_icmpv4_id buf id;
-      buf
-
-  end
 
   type t = {
     ip : IP.t;
