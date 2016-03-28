80630a156460afed914dc0a0b412d290eaa98ccc
Mindy Preston <mindy.preston@docker.com>
2016-03-28 19:07:28 +0100

    move icmp parse, print, wire, and impl to its own subdir
    Signed-off-by: Mindy Preston <mindy.preston@docker.com>

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,5 +1,5 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: b158237657691572fe0e65324f37e08c) *)
+(* DO NOT EDIT (digest: ba3ae7c3c8a8229a6afa9f2c8d480119) *)
 module OASISGettext = struct
 (* # 22 "src/oasis/OASISGettext.ml" *)
 
@@ -615,7 +615,7 @@ let package_default =
           ("arpv4", ["lib"], []);
           ("ipv4", ["lib"], []);
           ("ipv6", ["lib"], []);
-          ("icmpv4", ["lib"], []);
+          ("icmpv4", ["lib/icmp"], []);
           ("udp", ["lib"], []);
           ("tcp", ["tcp"], []);
           ("dhcpv4", ["dhcp"], []);
@@ -656,8 +656,9 @@ let package_default =
        [
           ("unix", ["lib"; "tcp"]);
           ("tcp", ["lib"]);
-          ("lib_test", ["lib"; "tcp"]);
-          ("lib", ["dhcp"; "tcp"]);
+          ("lib_test", ["lib"; "lib/icmp"; "tcp"]);
+          ("lib/icmp", ["lib"]);
+          ("lib", ["dhcp"; "lib/icmp"; "tcp"]);
           ("dhcp", ["lib"])
        ]
   }
@@ -667,7 +668,7 @@ let conf = {MyOCamlbuildFindlib.no_automatic_syntax = false}
 
 let dispatch_default = MyOCamlbuildBase.dispatch_default conf package_default;;
 
-# 671 "myocamlbuild.ml"
+# 672 "myocamlbuild.ml"
 (* OASIS_STOP *)
 Ocamlbuild_plugin.dispatch dispatch_default;;
 (* Ocamlbuild_pack.Flags.mark_tag_used "tests";; *)
