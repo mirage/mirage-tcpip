86833f458caa6bc82e8ea9bb003dc223c79c8692
Mindy Preston <mindy.preston@docker.com>
2016-03-31 14:25:18 +0100

    rename length-checking functions

--- lib/udp/udp_parse.ml
+++ lib/udp/udp_parse.ml
@@ -3,18 +3,18 @@ type t = {src_port: Cstruct.uint16; dst_port : Cstruct.uint16; payload : Cstruct
 let parse_udp_header buf =
   let open Rresult in
   let open Udp_wire in
-  let header_length_check () = 
+  let check_header_length () =
     if Cstruct.len buf < sizeof_udp then Error "UDP header too short" else Ok ()
   in
-  let payload_length_check hlen buflen () =
+  let check_payload_length hlen buflen () =
     let payload_len = buflen - hlen in
     if payload_len < 0
     then Error "UDP header claimed payload longer than it was"
     else Ok payload_len
   in
   let length = get_udp_length buf in
-  header_length_check () >>=
-  payload_length_check length (Cstruct.len buf) >>= fun payload_len ->
+  check_header_length () >>=
+  check_payload_length length (Cstruct.len buf) >>= fun payload_len ->
   let src_port = Udp_wire.get_udp_source_port buf in
   let dst_port = Udp_wire.get_udp_dest_port buf in
   let payload = Cstruct.sub buf Udp_wire.sizeof_udp payload_len in
