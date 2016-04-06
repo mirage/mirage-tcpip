c076eb557c041823d7ad5a99f7e1adc9012c5bd7
Mindy Preston <mindy.preston@docker.com>
2016-04-06 20:23:16 +0100

    use tcp_parse rather than tcp.wire (breaking rfc5961 tests in the process)

--- lib/tcp/wire.ml
+++ lib/tcp/wire.ml
@@ -20,15 +20,8 @@ let debug = Log.create "Wire"
 
 let count_tcp_to_ip = MProf.Counter.make ~name:"tcp-to-ip"
 
-let get_options buf =
-  if Tcp_wire.get_data_offset buf > 20 then
-    Options.unmarshal (Cstruct.shift buf Tcp_wire.sizeof_tcp) else []
-
 let set_options buf ts =
   Options.marshal buf ts
-
-let get_payload buf =
-  Cstruct.shift buf (Tcp_wire.get_data_offset buf)
 
 module Make (Ip:V1_LWT.IP) = struct
   type id = {
@@ -39,53 +32,21 @@ module Make (Ip:V1_LWT.IP) = struct
   }
 
   let xmit ~ip ~id ?(rst=false) ?(syn=false) ?(fin=false) ?(psh=false)
-      ~rx_ack ~seq ~window ~options datav =
+      ~rx_ack ~seq ~window ~options payload =
     (* Make a TCP/IP header frame *)
     let frame, header_len = Ip.allocate_frame ip ~dst:id.dest_ip ~proto:`TCP in
     (* Shift this out by the combined ethernet + IP header sizes *)
-    let tcp_frame = Cstruct.shift frame header_len in
-    (* Append the TCP options to the header *)
-    let options_frame = Cstruct.shift tcp_frame Tcp_wire.sizeof_tcp in
-    let options_len =
-      match options with
-      |[] -> 0
-      |options -> Options.marshal options_frame options
-    in
-    let tcp_frame = Cstruct.set_len tcp_frame (Tcp_wire.sizeof_tcp + options_len) in
-    (* At this point, extend the IPv4 view by the TCP+options size *)
-    let frame = Cstruct.set_len frame (header_len + Tcp_wire.sizeof_tcp + options_len) in
-    let sequence = Sequence.to_int32 seq in
-    let ack_number =
-      match rx_ack with Some n -> Sequence.to_int32 n |None -> 0l
-    in
-    let data_off = (Tcp_wire.sizeof_tcp / 4) + (options_len / 4) in
-    Tcp_wire.set_tcp_src_port tcp_frame id.local_port;
-    Tcp_wire.set_tcp_dst_port tcp_frame id.dest_port;
-    Tcp_wire.set_tcp_sequence tcp_frame sequence;
-    Tcp_wire.set_tcp_ack_number tcp_frame ack_number;
-    Tcp_wire.set_data_offset tcp_frame data_off;
-    Tcp_wire.set_tcp_flags tcp_frame 0;
-    if rx_ack <> None then Tcp_wire.set_ack tcp_frame;
-    if rst then Tcp_wire.set_rst tcp_frame;
-    if syn then Tcp_wire.set_syn tcp_frame;
-    if fin then Tcp_wire.set_fin tcp_frame;
-    if psh then Tcp_wire.set_psh tcp_frame;
-    Tcp_wire.set_tcp_window tcp_frame window;
-    Tcp_wire.set_tcp_checksum tcp_frame 0;
-    Tcp_wire.set_tcp_urg_ptr tcp_frame 0;
-    let checksum = Ip.checksum frame (tcp_frame :: datav) in
-    Tcp_wire.set_tcp_checksum tcp_frame checksum;
-    (* PERF: uncommenting the next expression results in ~10% perf degradation
-    Log.f debug (fun fmt ->
-        Log.pf fmt
-          "xmit checksum=%04x %a.%d->%a.%d rst=%b syn=%b fin=%b psh=%b \
-           seq=%lu ack=%lu options=%a datalen=%d datafrag=%d dataoff=%d olen=%d"
-          checksum
-          Ipaddr.pp_hum (Ip.to_uipaddr id.local_ip) id.local_port
-          Ipaddr.pp_hum (Ip.to_uipaddr id.dest_ip)  id.dest_port
-          rst syn fin psh sequence ack_number Options.pps options
-          (Cstruct.lenv datav) (List.length datav) data_off options_len); *)
-    MProf.Counter.increase count_tcp_to_ip (Cstruct.lenv datav + (if syn then 1 else 0));
-    Ip.writev ip frame datav
+    let tcp_buf = Cstruct.shift frame header_len in
+    let pseudoheader = Ip.pseudoheader ip ~dst:id.dest_ip ~proto:`TCP (Cstruct.lenv payload) in
+    match Tcp_print.print_tcp_header ~tcp_buf ~src_port:id.local_port
+      ~dst_port:id.dest_port ~seq ~rx_ack ~pseudoheader ~options ~syn ~rst ~fin
+      ~psh ~window ~payload with
+    | Error s ->
+      Log.f debug (fun fmt -> Log.pf fmt "Error transmitting TCP packet: %s" s);
+      Lwt.return_unit
+    | Ok len -> 
+      let frame = Cstruct.set_len frame (header_len + len) in
+      MProf.Counter.increase count_tcp_to_ip (Cstruct.lenv payload + (if syn then 1 else 0));
+      Ip.writev ip frame payload
 
 end
