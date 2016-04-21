23d5ab3a4ecc5a4cd1365fda3011dd63741f70eb
Mindy Preston <mindy.preston@docker.com>
2016-04-21 15:23:37 +0100

    use icmpv4 type cenum rather than an int in icmpv4_parse.t
    additionally, link icmpv4 parser against rresult and only parse known
    icmp message types.
    
    Signed-off-by: Mindy Preston <mindy.preston@docker.com>
    
    Conflicts:
    	_oasis
    	_tags
    	lib/META
    	lib/icmp/icmpv4_parse.ml
    	setup.ml

--- lib/icmp/icmpv4_parse.ml
+++ lib/icmp/icmpv4_parse.ml
@@ -1,14 +1,15 @@
-
+open Icmpv4_wire
 (* second 4 bytes of the message have varying interpretations *)
 type subheader =
   | Id_and_seq of Cstruct.uint16 * Cstruct.uint16
+  | Next_hop_mtu of Cstruct.uint16
   | Pointer of Cstruct.uint8
   | Address of Ipaddr.V4.t
   | Unused
 
 type t = {
   code : Cstruct.uint8;
-  ty : Cstruct.uint8;
+  ty : ty;
   csum : Cstruct.uint16;
   subheader : subheader;
   payload : Cstruct.t option;
@@ -17,28 +18,34 @@ type t = {
 let subheader_of_cstruct ty buf =
   let open Cstruct.BE in
   match ty with
-  | 0 | 8 | 13 | 14 | 15 | 16 -> Id_and_seq (get_uint16 buf 0, get_uint16 buf 2)
-  | 3 | 11 | 4 -> Unused
-  | 5 -> Address (Ipaddr.V4.of_int32 (get_uint32 buf 0))
-  | 12 -> Pointer (Cstruct.get_uint8 buf 0)
-  | _ -> Unused
+  | Echo_request | Echo_reply
+  | Timestamp_request | Timestamp_reply
+  | Information_request | Information_reply ->
+    Id_and_seq (get_uint16 buf 0, get_uint16 buf 2)
+  | Destination_unreachable -> Next_hop_mtu (get_uint16 buf 2)
+  | Time_exceeded
+  | Source_quench -> Unused
+  | Redirect -> Address (Ipaddr.V4.of_int32 (get_uint32 buf 0))
+  | Parameter_problem -> Pointer (Cstruct.get_uint8 buf 0)
 
 let input buf =
-  let open Icmpv4_wire in
+  let open Rresult in
+  let check_len () =
   if Cstruct.len buf < sizeof_icmpv4 then
     Result.Error "packet too short for ICMPv4 header"
-  else begin
-    try
-    let ty = get_icmpv4_ty buf in
-    let code = get_icmpv4_code buf in
-    let csum = get_icmpv4_csum buf in
-    let subheader = subheader_of_cstruct ty (Cstruct.shift buf 4) in
-    let payload =
-      if Cstruct.len buf > sizeof_icmpv4
-      then Some (Cstruct.shift buf sizeof_icmpv4)
-      else None
-    in
-    Result.Ok { code; ty; csum; subheader; payload }
-    with
-    | Invalid_argument s -> Result.Error s
-  end
+  else Result.Ok () in
+  let check_ty () =
+    match int_to_ty (get_icmpv4_ty buf) with
+    | None -> Result.Error "unrecognized ICMPv4 type"
+    | Some ty -> Result.Ok ty
+  in
+  check_len () >>= check_ty >>= fun ty ->
+  let code = get_icmpv4_code buf in
+  let csum = get_icmpv4_csum buf in
+  let subheader = subheader_of_cstruct ty (Cstruct.shift buf 4) in
+  let payload =
+    if Cstruct.len buf > sizeof_icmpv4
+    then Some (Cstruct.shift buf sizeof_icmpv4)
+    else None
+  in
+  Result.Ok { code; ty; csum; subheader; payload }
