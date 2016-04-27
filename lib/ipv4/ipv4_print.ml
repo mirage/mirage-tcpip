606883be1a279f51740b9f71ade0547037734751
Mindy Preston <mindy.preston@docker.com>
2016-04-27 21:52:55 +0100

    use ethif_print in arp

--- lib/ipv4/ipv4_print.ml
+++ lib/ipv4/ipv4_print.ml
@@ -1,3 +1,5 @@
+open Ipv4_wire
+
 let protocol_to_int = function
   | `ICMP   -> 1
   | `TCP    -> 6
@@ -13,3 +15,17 @@ let pseudoheader ~src ~dst ~proto len =
   Cstruct.set_uint8 ph 9 proto;
   Cstruct.BE.set_uint16 ph 10 len;
   ph
+
+let print_ipv4_header ~buf ~src ~dst ~proto ~ttl =
+  if Cstruct.len buf < sizeof_ipv4 then
+    Result.Error "Not enough space for IPv4 header"
+  else begin
+    set_ipv4_hlen_version buf ((4 lsl 4) + (5));
+    set_ipv4_tos buf 0;
+    set_ipv4_off buf 0; (* TODO fragmentation *)
+    set_ipv4_ttl buf ttl;
+    set_ipv4_proto buf (protocol_to_int proto);
+    set_ipv4_src buf (Ipaddr.V4.to_int32 src);
+    set_ipv4_dst buf (Ipaddr.V4.to_int32 dst);
+    Result.Ok ()
+  end
