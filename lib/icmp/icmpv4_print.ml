31bb43efee14df04fe054c0d8e14c82855bfc36f
Mindy Preston <mindy.preston@docker.com>
2016-04-28 16:02:06 +0100

    use moar combinators

--- lib/icmp/icmpv4_print.ml
+++ lib/icmp/icmpv4_print.ml
@@ -1,24 +1,30 @@
 open Icmpv4_wire
 
-let echo ?payload ty id seq =
-  let header = Cstruct.create sizeof_icmpv4 in
-  set_icmpv4_ty header (ty_to_int ty);
-  set_icmpv4_code header 0x00;
-  set_icmpv4_csum header 0x0000;
-  set_icmpv4_seq header seq;
-  set_icmpv4_id header id;
-  let packet = match payload with
-    | Some payload -> Cstruct.append header payload
-    | None -> header
-  in
-  set_icmpv4_csum header (Tcpip_checksum.ones_complement_list [ packet ]);
-  packet
+let echo ~buf ?payload ~ty ~id ~seq =
+  let min_len = sizeof_icmpv4 + (match payload with | None -> 0 | Some payload ->
+      Cstruct.len payload) in
+  if Cstruct.len buf < min_len then
+    Result.Error "Not enough space for ICMP header and payload"
+  else begin
+    let buf = Cstruct.set_len buf min_len in
+    set_icmpv4_ty buf (ty_to_int ty);
+    set_icmpv4_code buf 0x00;
+    set_icmpv4_csum buf 0x0000;
+    set_icmpv4_seq buf seq;
+    set_icmpv4_id buf id;
+    let packets = match payload with
+      | Some payload -> [buf ; payload]
+      | None -> [buf]
+    in
+    set_icmpv4_csum buf (Tcpip_checksum.ones_complement_list packets);
+    Result.Ok ()
+  end
 
-let echo_request ?payload id seq =
-  echo ?payload Echo_request id seq
+let echo_request ~buf ?payload ~id ~seq =
+  echo ~buf ?payload ~ty:Echo_request ~id ~seq
 
-let echo_reply ?payload id seq =
-  echo ?payload Echo_reply id seq
+let echo_reply ~buf ?payload ~id ~seq =
+  echo ~buf ?payload ~ty:Echo_reply ~id ~seq
 
 (** [would_fragment ip_header ip_payload next_hop_mtu] generates an
     ICMP destination unreachable message, with the code set to 4 ("packet
@@ -26,15 +32,8 @@ let echo_reply ?payload id seq =
     be the IP header of the packet which will be rejected; [ip_payload] is the
     optional first 8 bytes of the IPv4 payload (generally the first 8 bytes of
     a UDP or TCP header).  Payloads of length > 8 will be truncated. *)
-let would_fragment ~ip_header ?ip_payload ~next_hop_mtu =
+let would_fragment ~buf ~ip_header ?ip_payload ~next_hop_mtu =
   (* type 3, code 4 *)
-  let header = Cstruct.create sizeof_icmpv4 in
-  set_icmpv4_ty header (ty_to_int Destination_unreachable);
-  set_icmpv4_code header (unreachable_reason_to_int Would_fragment);
-  set_icmpv4_csum header 0x0000;
-  (* this field is unused for icmp destination unreachable *)
-  set_icmpv4_id header 0x00;
-  set_icmpv4_seq header next_hop_mtu;
   let icmp_payload = match ip_payload with
     | Some ip_payload ->
       if (Cstruct.len ip_payload > 8) then begin
@@ -43,6 +42,17 @@ let would_fragment ~ip_header ?ip_payload ~next_hop_mtu =
       end else Cstruct.append ip_header ip_payload
     | None -> ip_header
   in
-  let icmp_packet = Cstruct.append header icmp_payload in
-  set_icmpv4_csum header (Tcpip_checksum.ones_complement_list [ icmp_packet ]);
-
+  if (Cstruct.len buf < sizeof_icmpv4 + Cstruct.len icmp_payload) then
+    Result.Error "buf is not large enough for icmp header and payload"
+  else begin
+    let header = Cstruct.create sizeof_icmpv4 in
+    set_icmpv4_ty header (ty_to_int Destination_unreachable);
+    set_icmpv4_code header (unreachable_reason_to_int Would_fragment);
+    set_icmpv4_csum header 0x0000;
+    (* this field is unused for icmp destination unreachable *)
+    set_icmpv4_id header 0x00;
+    set_icmpv4_seq header next_hop_mtu;
+    let icmp_packet = Cstruct.append header icmp_payload in
+    set_icmpv4_csum header (Tcpip_checksum.ones_complement_list [ icmp_packet ]);
+    Result.Ok ()
+  end
