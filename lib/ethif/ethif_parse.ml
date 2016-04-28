a0a6cae9e48239079859f15a5cfa50a55cad8286
Mindy Preston <mindy.preston@docker.com>
2016-04-28 13:47:05 +0100

    make ethif_parse fit in better with the _parse family of products

--- lib/ethif/ethif_parse.ml
+++ lib/ethif/ethif_parse.ml
@@ -1,12 +1,21 @@
-let parse_ethernet_frame frame =
-  let open Ethif_wire in
-  if Cstruct.len frame >= 14 then
-    (* source + destination + type = 14 *)
-    let payload = Cstruct.shift frame sizeof_ethernet
-    and typ = get_ethernet_ethertype frame
-    and dst = Macaddr.of_bytes_exn (copy_ethernet_dst frame)
-    in
-    Some (int_to_ethertype typ, dst, payload)
-  else
-    None
+open Ethif_wire
 
+type t = {
+  source : Macaddr.t;
+  destination : Macaddr.t;
+  ethertype : Ethif_wire.ethertype;
+  payload : Cstruct.t; (* bare ethernet frames not allowed *)
+}
+
+let parse_ethernet_header frame =
+  if Cstruct.len frame >= sizeof_ethernet then
+    match get_ethernet_ethertype frame |> int_to_ethertype with
+    | None -> Result.Error "unknown ethertype in frame"
+    | Some ethertype ->
+      let payload = Cstruct.shift frame sizeof_ethernet
+      and source = Macaddr.of_bytes_exn (copy_ethernet_src frame)
+      and destination = Macaddr.of_bytes_exn (copy_ethernet_dst frame)
+      in
+      Result.Ok { destination; source; ethertype; payload }
+  else
+    Result.Error "frame too small to contain a valid ethernet header"
