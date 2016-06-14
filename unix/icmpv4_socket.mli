0377d39da615d952c55e305cf3f580708f5a7272
Mindy Preston <mindy.preston@docker.com>
2016-06-14 16:46:28 +0100

    fix type declaration for icmpv4_socket.connect

--- unix/icmpv4_socket.mli
+++ unix/icmpv4_socket.mli
@@ -2,4 +2,4 @@ include V1_LWT.ICMP with type ipaddr = Ipaddr.V4.t
                      and type buffer = Cstruct.t
                      and type 'a io = 'a Lwt.t
 
-val connect : unit -> unit io
+val connect : unit -> t io
