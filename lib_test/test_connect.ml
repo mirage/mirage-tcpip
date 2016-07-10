33f2d21bc17f761265c409a6c74d350e4ca1dcf1
Mindy Preston <mindy.preston@docker.com>
2016-07-10 13:05:40 +0100

    remove console from tests as well
    Conflicts:
    	lib_test/test_iperf.ml
    	lib_test/test_rfc5961.ml

--- lib_test/test_connect.ml
+++ lib_test/test_connect.ml
@@ -19,8 +19,10 @@ open Vnetif_common
 
 let (>>=) = Lwt.(>>=)
 
+let src = Logs.Src.create "test_connect" ~doc:"connect tests"
+module Log = (val Logs.src_log src : Logs.LOG)
+
 module Test_connect (B : Vnetif_backends.Backend) = struct
-  module C = Console
   module V = VNETIF_STACK (B)
 
   let netmask = Ipaddr.V4.of_string_exn "255.255.255.0"
@@ -29,8 +31,6 @@ module Test_connect (B : Vnetif_backends.Backend) = struct
   let server_ip = Ipaddr.V4.of_string_exn "10.0.0.100"
   let test_string = "Hello world from Mirage 123456789...."
   let backend = V.create_backend ()
-
-  let log_s c fmt = Printf.ksprintf (C.log_s c) (fmt ^^ "%!")
 
   let err_read_eof () = fail "accept got EOF while reading"
   let err_write_eof () = fail "client tried to write, got EOF"
@@ -43,10 +43,9 @@ module Test_connect (B : Vnetif_backends.Backend) = struct
     let err = V.Stackv4.TCPV4.error_message e in
     fail "client tried to write, got %s" err
 
-  let accept c flow expected =
+  let accept flow expected =
     let ip, port = V.Stackv4.TCPV4.dst flow in
-    log_s c "Accepted connection from %s:%d" (Ipaddr.V4.to_string ip) port
-    >>= fun () ->
+    Logs.debug (fun f -> f "Accepted connection from %s:%d" (Ipaddr.V4.to_string ip) port);
     V.Stackv4.TCPV4.read flow >>= function
     | `Eof     -> err_read_eof ()
     | `Error e -> err_read e
@@ -54,29 +53,29 @@ module Test_connect (B : Vnetif_backends.Backend) = struct
       OS.Time.sleep 0.1 >>= fun () ->
       (* sleep first to capture data in pcap *)
       assert_string "accept" expected (Cstruct.to_string b);
-      log_s c "Connection closed"
+      Logs.debug (fun f -> f "Connection closed");
+      Lwt.return_unit
 
   let test_tcp_connect_two_stacks () =
-    or_error "console" Console.connect "console" >>= fun c ->
     let timeout = 15.0 in
     Lwt.pick [
       (Lwt_unix.sleep timeout >>= fun () ->
        fail "connect test timedout after %f seconds" timeout) ;
 
-      (V.create_stack c backend server_ip netmask [gw] >>= fun s1 ->
-       V.Stackv4.listen_tcpv4 s1 ~port:80 (fun f -> accept c f test_string);
+      (V.create_stack backend server_ip netmask [gw] >>= fun s1 ->
+       V.Stackv4.listen_tcpv4 s1 ~port:80 (fun f -> accept f test_string);
        V.Stackv4.listen s1) ;
 
       (Lwt_unix.sleep 0.1 >>= fun () ->
-       V.create_stack c backend client_ip netmask [gw] >>= fun s2 ->
+       V.create_stack backend client_ip netmask [gw] >>= fun s2 ->
        let conn = V.Stackv4.TCPV4.create_connection (V.Stackv4.tcpv4 s2) in
        or_error "connect" conn (server_ip, 80) >>= fun flow ->
-       log_s c "Connected to other end..." >>= fun () ->
+       Logs.debug (fun f -> f "Connected to other end...");
        V.Stackv4.TCPV4.write flow (Cstruct.of_string test_string) >>= function
        | `Error e -> err_write e
        | `Eof     -> err_write_eof ()
        | `Ok ()   ->
-         log_s c "wrote hello world" >>= fun () ->
+         Logs.debug (fun f -> f "wrote hello world");
          V.Stackv4.TCPV4.close flow >>= fun () ->
          Lwt_unix.sleep 1.0 >>= fun () -> (* record some traffic after close *)
          Lwt.return_unit) ] >>= fun () ->
