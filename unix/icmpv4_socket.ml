3bb90ee42f6dcd6120b7e1260ec6e550c56c1cf0
Mindy Preston <mindy.preston@docker.com>
2016-07-11 14:39:32 +0100

    fixes warnings & adds more logging in icmpv4_socket.ml

--- unix/icmpv4_socket.ml
+++ unix/icmpv4_socket.ml
@@ -27,7 +27,7 @@ let pp_sockaddr fmt sa =
 let src = Logs.Src.create "icmpv4_socket" ~doc:"Mirage ICMPv4 (Sockets Edition)"
 module Log = (val Logs.src_log src : Logs.LOG)
 
-let write t ~dst buf =
+let write _t ~dst buf =
   let open Lwt_unix in
   let flags = [] in
   let ipproto_icmp = 1 in (* according to BSD /etc/protocols *)
@@ -35,36 +35,36 @@ let write t ~dst buf =
   let fd = socket PF_INET SOCK_DGRAM ipproto_icmp in
   let in_addr = Unix.inet_addr_of_string (Ipaddr.V4.to_string dst) in
   let sockaddr = ADDR_INET (in_addr, port) in
-  Lwt_cstruct.sendto fd buf flags sockaddr >>= fun _sent ->
-  (* TODO: log short reads? *)
+  Lwt_cstruct.sendto fd buf flags sockaddr >>= fun sent ->
+          Log.debug (fun f -> f "short write: %d received vs %d expected" sent (Cstruct.len buf));
   Lwt_unix.close fd
 
-let input t ~src ~dst buf =
+let input t ~src ~dst:_ buf =
   (* some default logic -- respond to echo requests with echo replies *)
   match Icmpv4_packet.Unmarshal.of_cstruct buf with
-  | Error _ -> (* TODO: log error *) Lwt.return_unit
+  | Result.Error s -> Logs.debug (fun f -> f "Error decomposing an ICMP packet: %s" s); Lwt.return_unit
   | Result.Ok (icmp, payload) ->
+    let open Icmpv4_packet in
     match icmp.ty, icmp.subheader with
-    | Echo_request, Id_and_seq (id, seq) ->
-      let response = Icmpv4_packet.(
+    | Icmpv4_wire.Echo_request, Id_and_seq (id, seq) ->
+      let response =
           { ty = Icmpv4_wire.Echo_reply;
             code = 0x00;
-            subheader = Id_and_seq (id, seq); }) in
-      write t ~dst:src (Icmpv4_packet.Marshal.make_cstruct response ~payload)
+            subheader = Id_and_seq (id, seq); } in
+      write t ~dst:src (Marshal.make_cstruct response ~payload)
     | _, _ -> Lwt.return_unit
 
-let listen t addr fn =
-  let open Lwt_unix in
-  let fd = socket PF_INET SOCK_DGRAM ipproto_icmp in
-  let sa = ADDR_INET (Unix.inet_addr_of_string (Ipaddr.V4.to_string addr), port) in
-  let () = bind fd sa in
+let listen _t addr fn =
+  let fd = Lwt_unix.(socket PF_INET SOCK_DGRAM) ipproto_icmp in
+  let sa = Lwt_unix.ADDR_INET (Unix.inet_addr_of_string (Ipaddr.V4.to_string addr), port) in
+  let () = Lwt_unix.bind fd sa in
   Log.debug (fun f -> f "Bound ICMP file descriptor to %a" pp_sockaddr sa);
-  let rec aux fn = 
+  let aux fn = 
     let receive_buffer = Cstruct.create 4096 in
-    Lwt_cstruct.recvfrom fd receive_buffer [] >>= fun (len, sockaddr) ->
+    Lwt_cstruct.recvfrom fd receive_buffer [] >>= fun (len, _sockaddr) ->
     (* trim the buffer to the amount of data actually received *)
     let receive_buffer = Cstruct.set_len receive_buffer len in
     fn receive_buffer
   in
-  aux fn >>= fun () -> close fd
+  aux fn >>= fun () -> Lwt_unix.close fd
 
