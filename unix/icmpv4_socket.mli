3774cea7ae4639b51396ac9dbbda6f70a7ee0ccd
Mindy Preston <mindy.preston@docker.com>
2016-07-11 14:39:48 +0100

    exposes listen in icmpv4_socket.mli

--- unix/icmpv4_socket.mli
+++ unix/icmpv4_socket.mli
@@ -3,3 +3,7 @@ include V1_LWT.ICMP with type ipaddr = Ipaddr.V4.t
                      and type 'a io = 'a Lwt.t
 
 val connect : unit -> t io
+
+val listen : t -> ipaddr -> (buffer -> unit io) -> unit io
+(** [listen t addr fn] attempts to create an unprivileged listener on IP address [addr].
+ * It will take any incoming ICMP packets and process them with the provided [fn]. *)
