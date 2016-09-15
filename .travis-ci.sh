4048643c86015d7ad768a28e472bd98024dc96f0
Anil Madhavapeddy <anil@recoil.org>
2016-09-15 21:10:52 +0100

    debug weird travis failure

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,5 +3,5 @@
 opam install -y mirage
 git clone -b mirage-dev git://github.com/mirage/mirage-skeleton
 cd mirage-skeleton
-make MODE=unix && make clean
 make MODE=xen && make clean
+make MODE=unix && make clean
