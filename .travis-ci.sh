34ca56848fb661e2658dc3c5225360eb4b6a8be7
Anil Madhavapeddy <anil@recoil.org>
2016-09-15 11:03:51 +0100

    build mirage-skeleton in tests instead of mirage-www

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,12 +1,6 @@
 # default tests
 
 opam install -y mirage
-git clone git://github.com/mirage/mirage-www
-cd mirage-www
-
-for mode in unix xen; do
-  export OPAMYES=1
-  eval `opam config env`
-  make MODE=$mode configure
-  make MODE=$mode build
-done
+git clone -b mirage-dev git://github.com/mirage/mirage-skeleton
+cd mirage-skeleton
+make all
