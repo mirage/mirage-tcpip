929942505a1fc8af5623cdf91ce11d3bbcc7a656
Anil Madhavapeddy <anil@recoil.org>
2016-09-16 11:54:04 +0100

    try setting MINIOS_COMPILE_ARCH to be uname insensitive

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,5 +3,6 @@
 opam install -y mirage
 git clone -b mirage-dev git://github.com/mirage/mirage-skeleton
 cd mirage-skeleton
+export MINIOS_COMPILE_ARCH=x86_64
 make MODE=xen && make clean
 make MODE=unix && make clean
