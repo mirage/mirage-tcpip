6cd075512fd9b29411bbb744faeb1e8d55da4570
Hannes Mehnert <hannes@mehnert.org>
2018-06-02 21:56:56 +0200

    Tcp_socket: avoid raising an exception (`Lwt.fail exn`) when `write` errors,   instead use the variant ``[ `Exn of exn ]`` in (`Lwt.return (Error (`Exn exn))`)   as done in `read` Tcpv4_socket / Tcpv6_socket: expose the custom variants in `type error` and   `type write_error` in the interface

--- src/stack-unix/tcpv6_socket.mli
+++ src/stack-unix/tcpv6_socket.mli
@@ -15,9 +15,12 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-include Mirage_protocols_lwt.TCP with type ip = Ipaddr.V6.t option
-                    and type ipaddr = Ipaddr.V6.t
-                    and type ipinput = unit Lwt.t
-                    and type flow = Lwt_unix.file_descr
+include Mirage_protocols_lwt.TCP
+  with type ip = Ipaddr.V6.t option
+   and type ipaddr = Ipaddr.V6.t
+   and type ipinput = unit Lwt.t
+   and type flow = Lwt_unix.file_descr
+   and type error = [ Mirage_protocols.Tcp.error | `Exn of exn ]
+   and type write_error = [ Mirage_protocols.Tcp.write_error | `Exn of exn ]
 
 val connect : ip -> t io
