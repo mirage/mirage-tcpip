f00a74b1361808c401b01442d0b13e120193a2e4
Hannes Mehnert <hannes@mehnert.org>
2018-08-29 23:45:50 +0200

    IPV4 no longer has an ethif nor prefix type

--- src/ipv4/static_ipv4.mli
+++ src/ipv4/static_ipv4.mli
@@ -14,13 +14,13 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-module Make (N:Mirage_protocols_lwt.ETHIF) (A: Mirage_protocols_lwt.ARP) : sig
-  include Mirage_protocols_lwt.IPV4 with type ethif = N.t
+module Make (E:Mirage_protocols_lwt.ETHIF) (A: Mirage_protocols_lwt.ARP) : sig
+  include Mirage_protocols_lwt.IPV4
   val connect :
     ?ip:Ipaddr.V4.t ->
     ?network:Ipaddr.V4.Prefix.t ->
     ?gateway:Ipaddr.V4.t option ->
-    ethif -> A.t -> t Lwt.t
+    E.t -> A.t -> t Lwt.t
     (** Connect to an ipv4 device.
         Default ip is {!Ipaddr.V4.any}
         Default network is {!Ipaddr.V4.any}/0
