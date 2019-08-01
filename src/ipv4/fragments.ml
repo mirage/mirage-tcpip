0ec179eeef8f70852bd137da637cfd6ecb8860f1
Hannes Mehnert <hannes@mehnert.org>
2019-08-01 14:26:50 +0200

    address @cfcs comments

--- src/ipv4/fragments.ml
+++ src/ipv4/fragments.ml
@@ -183,12 +183,13 @@ let process cache ts (packet : Ipv4_packet.t) payload =
 
 (* TODO hdr.options is a Cstruct.t atm, but instead we need to parse all the
    options, and distinguish based on the first bit -- only these with the bit
-   set should be copied into all fragments (see RFC 791) *)
+   set should be copied into all fragments (see RFC 791, 3.1, page 15) *)
 let fragment ~mtu hdr payload =
   let rec frag1 acc hdr hdr_buf offset data_size payload =
     let more = Cstruct.len payload > data_size in
     let hdr' =
-      let off = offset / 8 lor (if more then 0x2000 else 0) in
+      (* off is 16 bit of IPv4 header, 0x2000 sets the more fragments bit *)
+      let off = (offset / 8) lor (if more then 0x2000 else 0) in
       { hdr with Ipv4_packet.off }
     in
     let this_payload, rest =
@@ -197,6 +198,8 @@ let fragment ~mtu hdr payload =
     let payload_len = Cstruct.len this_payload in
     Ipv4_wire.set_ipv4_csum hdr_buf 0;
     (match Ipv4_packet.Marshal.into_cstruct ~payload_len hdr' hdr_buf with
+     (* hdr_buf is allocated with hdr_size (computed below) bytes, thus
+        into_cstruct will never return an error! *)
      | Error msg -> invalid_arg msg
      | Ok () -> ());
     let acc' = Cstruct.append hdr_buf this_payload :: acc in
