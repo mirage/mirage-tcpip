fe4b93f3cb88d83e1a21f86e9a3c020e5a0d739f
phaer <hello@phaer.org>
2019-09-26 18:29:35 +0200

    Add ttl parameter to Icmpv4_socket.write.

--- src/stack-unix/icmpv4_socket.ml
+++ src/stack-unix/icmpv4_socket.ml
@@ -49,7 +49,7 @@ let recvfrom' fd buf flags =
     Lwt.return (n, sockaddr)
   end else Lwt_cstruct.recvfrom fd buf flags
 
-let write _t ~dst buf =
+let write _t ~dst ?ttl:_ttl buf =
   let open Lwt_unix in
   let flags = [] in
   let ipproto_icmp = 1 in (* according to BSD /etc/protocols *)
