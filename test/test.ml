3ccba30b9d0969740b6c7d8b47ea76d9e7f14ae5
Magnus Skjegstad <magnus@skjegstad.com>
2020-08-31 14:32:48 +0200

    Add iperf ipv6 test
    This duplicates the iperf test for ipv6. As the V4 stack signatures
    contain a reference to the IP version (e.g. listen_tcpv4 etc) it was
    difficult to reuse the test without major changes. We should clean this
    up when we unify the signatures to avoid duplicated tests for IPv4/IPv6.

--- test/test.ml
+++ test/test.ml
@@ -29,6 +29,7 @@ let suite = [
   "connect_ipv6"        , Test_connect_ipv6.suite     ;
   "deadlock"       , Test_deadlock.suite    ;
   "iperf"          , Test_iperf.suite       ;
+  "iperf_ipv6"     , Test_iperf_ipv6.suite       ;
   "keepalive"      , Test_keepalive.suite   ;
 ]
 
