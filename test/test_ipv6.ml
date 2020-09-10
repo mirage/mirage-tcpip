83ef9db71c4719ac17bcfd989e478b271ab41439
Hannes Mehnert <hannes@mehnert.org>
2020-09-10 23:10:09 +0200

    test: ipv6 stack initialization failure if NA are received (DAD failure)

--- test/test_ipv6.ml
+++ test/test_ipv6.ml
@@ -92,7 +92,8 @@ let create_ethernet backend =
           ~arpv4:(fun _ -> Lwt.return_unit)
           ~ipv4:(fun _ -> Lwt.return_unit)
           ~ipv6) >|= fun _ -> ()),
-  (fun dst ?size f -> E.write ethif dst `IPv6 ?size f)
+  (fun dst ?size f -> E.write ethif dst `IPv6 ?size f),
+  E.mac ethif
 
 let solicited_node_prefix =
   Ipaddr.V6.(Prefix.make 104 (of_int16 (0xff02, 0, 0, 0, 0, 1, 0xff00, 0)))
@@ -101,7 +102,7 @@ let dad_na_is_sent () =
   let address = Ipaddr.V6.of_string_exn "fc00::23" in
   let backend = B.create () in
   get_stack backend address >>= fun stack ->
-  create_ethernet backend >>= fun (listen_raw, write_raw) ->
+  create_ethernet backend >>= fun (listen_raw, write_raw, _) ->
   let received_one, on_received_one = Lwt.task () in
   let nd_size = Ipv6_wire.sizeof_ipv6 + Ipv6_wire.sizeof_ns in
   let nd buf =
@@ -111,13 +112,13 @@ let dad_na_is_sent () =
     Ipaddr_cstruct.V6.write_cstruct_exn (Ipaddr.V6.Prefix.network_address solicited_node_prefix address) (Cstruct.shift buf 24);
     Ipv6_wire.set_ipv6_hlim buf 255;
     Ipv6_wire.set_ipv6_nhdr buf (Ipv6_wire.protocol_to_int `ICMP);
-    let icmpbuf = Cstruct.shift buf Ipv6_wire.sizeof_ipv6 in
+    let hdr, icmpbuf = Cstruct.split buf Ipv6_wire.sizeof_ipv6 in
     Ipv6_wire.set_ns_ty icmpbuf 135; (* NS *)
     Ipv6_wire.set_ns_code icmpbuf 0;
     Ipv6_wire.set_ns_reserved icmpbuf 0l;
-    Ipaddr_cstruct.V6.write_cstruct_exn address (Cstruct.shift icmpbuf 6);
+    Ipaddr_cstruct.V6.write_cstruct_exn address (Cstruct.shift icmpbuf 8);
     Ipv6_wire.set_icmpv6_csum icmpbuf 0;
-    Ipv6_wire.set_icmpv6_csum icmpbuf @@ Ndpv6.checksum buf [];
+    Ipv6_wire.set_icmpv6_csum icmpbuf @@ Ndpv6.checksum hdr [icmpbuf];
     nd_size
   and is_na buf =
     let icmpbuf = Cstruct.shift buf Ipv6_wire.sizeof_ipv6 in
@@ -130,8 +131,11 @@ let dad_na_is_sent () =
       Ipaddr.V6.link_nodes = 0 &&
     Ipv6_wire.get_ipv6_hlim buf = 255 &&
     Ipv6_wire.get_ipv6_nhdr buf = Ipv6_wire.protocol_to_int `ICMP &&
-    Ipv6_wire.get_ns_ty icmpbuf = 136 &&
-    Ipv6_wire.get_ns_code icmpbuf = 0
+    Ipv6_wire.get_na_ty icmpbuf = 136 &&
+    Ipv6_wire.get_na_code icmpbuf = 0 &&
+    Ipaddr.V6.compare
+      (Ipaddr_cstruct.V6.of_cstruct_exn (Cstruct.shift icmpbuf 8))
+      address = 0
   in
   Lwt.pick [
     listen stack;
@@ -145,7 +149,76 @@ let dad_na_is_sent () =
      Alcotest.fail "NA packet should have been received")
   ]
 
+let multicast_mac =
+  let pbuf = Cstruct.create 6 in
+  Cstruct.BE.set_uint16 pbuf 0 0x3333;
+  fun ip ->
+    let _, _, _, n = Ipaddr.V6.to_int32 ip in
+    Cstruct.BE.set_uint32 pbuf 2 n;
+    Macaddr_cstruct.of_cstruct_exn pbuf
+
+let dad_na_is_received () =
+  let address = Ipaddr.V6.of_string_exn "fc00::23" in
+  let backend = B.create () in
+  create_ethernet backend >>= fun (listen_raw, write_raw, mac) ->
+  let na_size = Ipv6_wire.sizeof_ipv6 + Ipv6_wire.sizeof_na + Ipv6_wire.sizeof_llopt in
+  let is_ns buf =
+    let icmpbuf = Cstruct.shift buf Ipv6_wire.sizeof_ipv6 in
+    if
+      Ipv6_wire.get_ipv6_version_flow buf = 0x60000000l && (* IPv6 *)
+      Ipaddr.V6.compare
+        (Ipaddr_cstruct.V6.of_cstruct_exn (Cstruct.shift buf 8))
+        Ipaddr.V6.unspecified = 0 &&
+      Ipaddr.V6.Prefix.mem
+        (Ipaddr_cstruct.V6.of_cstruct_exn (Cstruct.shift buf 24))
+        solicited_node_prefix &&
+      Ipv6_wire.get_ipv6_hlim buf = 255 &&
+      Ipv6_wire.get_ipv6_nhdr buf = Ipv6_wire.protocol_to_int `ICMP &&
+      Ipv6_wire.get_ns_ty icmpbuf = 135 &&
+      Ipv6_wire.get_ns_code icmpbuf = 0
+    then
+      Some (Ipaddr_cstruct.V6.of_cstruct_exn (Cstruct.shift icmpbuf 8))
+    else
+      None
+  in
+  let na addr buf =
+    Ipv6_wire.set_ipv6_version_flow buf 0x60000000l; (* IPv6 *)
+    Ipv6_wire.set_ipv6_len buf (Ipv6_wire.sizeof_na + Ipv6_wire.sizeof_llopt);
+    Ipaddr_cstruct.V6.write_cstruct_exn addr (Cstruct.shift buf 8);
+    Ipaddr_cstruct.V6.write_cstruct_exn Ipaddr.V6.link_nodes (Cstruct.shift buf 24);
+    Ipv6_wire.set_ipv6_hlim buf 255;
+    Ipv6_wire.set_ipv6_nhdr buf (Ipv6_wire.protocol_to_int `ICMP);
+    let hdr, icmpbuf = Cstruct.split buf Ipv6_wire.sizeof_ipv6 in
+    Ipv6_wire.set_na_ty icmpbuf 136; (* NA *)
+    Ipv6_wire.set_na_code icmpbuf 0;
+    Ipv6_wire.set_na_reserved icmpbuf 0x20000000l;
+    Ipaddr_cstruct.V6.write_cstruct_exn addr (Cstruct.shift icmpbuf 8);
+    let optbuf = Cstruct.shift icmpbuf Ipv6_wire.sizeof_na in
+    Ipv6_wire.set_llopt_ty optbuf 2;
+    Ipv6_wire.set_llopt_len optbuf 1;
+    Macaddr_cstruct.write_cstruct_exn mac (Cstruct.shift optbuf 2);
+    Ipv6_wire.set_icmpv6_csum icmpbuf 0;
+    Ipv6_wire.set_icmpv6_csum icmpbuf @@ Ndpv6.checksum hdr [icmpbuf];
+    na_size
+  in
+  Lwt.pick [
+    (listen_raw (fun buf ->
+         match is_ns buf with
+         | None -> Lwt.return_unit
+         | Some addr ->
+           let dst = multicast_mac Ipaddr.V6.link_nodes in
+           write_raw dst ~size:na_size (na addr) >|= fun _ -> ()));
+    (Lwt.catch
+       (fun () -> get_stack backend address >|= fun _ -> Error ())
+       (fun _ -> Lwt.return (Ok ())) >|= function
+     | Ok () -> ()
+     | Error () -> Alcotest.fail "Expected stack initialization failure");
+    (Time.sleep_ns (Duration.of_ms 5000) >>= fun () ->
+     Alcotest.fail "stack initialization should have failed")
+  ]
+
 let suite = [
   "Send a UDP packet from one IPV6 stack and check it is received by another", `Quick, pass_udp_traffic;
   "NA is sent when a ND is received", `Quick, dad_na_is_sent;
+  "NA is received, stack fails to initialise", `Quick, dad_na_is_received;
 ]
